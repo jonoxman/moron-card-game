a1261501d0f737563297b5cea6dd73c41a21138e
jonoxman <oxmanjonathan@gmail.com>
2023-06-28 20:57:47 -0400

    Implement human player's move input method. Fix sort_hand crash

--- main.py
+++ main.py
@@ -92,15 +92,16 @@ class Player:
         self.hand.sort(key=lambda card: card.rank) #Sort the hand in ascending order by rank
 
         trumps = []
-        for i in range(len(self.hand)):
+        i = 0 
+        # This is a little awkward. I don't want to use a usual for loop with range, because I want to decrement the index when necessary. It's either this, or a while loop.
+        # The length of the hand can never change in the middle of the sort, so this is good enough (and I believe, slightly faster).
+        for _ in itertools.repeat(None, self.num_cards()):
             if self.hand[i].is_trump: 
                 trumps.append(self.hand.pop(i))
-                i -= 1 # To avoid skipping a card
+            else:
+                i += 1
         
-        for c in trumps: #Place all trumps at the end
-            self.hand.append(c)
-
-
+        self.hand.extend(trumps) #Place all trumps at the end
         """ 
         # Sort by suits, and then sort by rank within each suit. 
         spades, hearts, clubs, diamonds = suitlist = [[] for _ in range(4)]
@@ -112,10 +113,6 @@ class Player:
 
         self.hand = list(itertools.chain.from_iterable(suitlist)) # Join suits back into one hand.  """
         
-
-
-
-
     def __str__(self):
         '''Overridden to output the player's hand, separated by new lines.'''
         return "\n".join(self.hand)
@@ -123,15 +120,57 @@ class Player:
 class HumanPlayer(Player):
     '''A HumanPlayer is a Player that can take user input for moves.'''
     def __init__(self, name):
-        super(self, name)
+        super().__init__(name)
 
-
-
+    def generate_move(self):
+        nl = "\n" # Escape sequences are not allowed in f-strings.
+        self.sort_hand() # This can be done here, since the only purpose of sorting your hand is for readability.
+        print(f"{self.name}, you have {self.num_cards()} cards. They are:\n{nl.join(f'{i + 1}. {self.hand[i]}' for i in range(self.num_cards()))}")
+        invalid_input = True
+        while invalid_input:
+            s = input("\nWhat would you like to play? (enter a number displayed above):\n")
+            if s.isdigit() and int(s) <= self.num_cards() and int(s) != 0:
+                invalid_input = False
+                break
+            
+            print("Invalid input.")
+        result = int(s) - 1
+        return self.hand[result]
+    
 class Game:
     '''
     A Game is a class representing an individual game. Games are the main class of this project. Each game has its own Deck, Players, and so on.
     The Game class is responsible for running the game, including taking user input. 
     '''
+
+    class RoundResult:
+        '''A RoundResult is a data class consisting of three elements:
+        1. The result of the round (i.e. which player "won", that is, which player plays next). This is a Player object.
+        2. The cards to be added to the attacker's hand after the round. If this is a number, the attacker draws this many cards. If it is a list of cards, each card is added.
+        3. The cards to be added to the defender's hand after the round. If this is a number, the defender draws this many cards. If it is a list of cards, each card is added. 
+        Note: Elements 2 and 3 can only be lists in the case of one player surrendering.
+        '''
+
+        def __init__(self, _winner, c1, c2):
+            self.winner = _winner
+            self.attacker_draw = c1
+            self.defender_draw = c2
+
+    class Round:
+        '''A Round is a class representing a single round of the game. The round keeps track of each player's hand, the cards on the field, and the valid moves each player can make.
+        When a round is initialized, the first player passed into it is the attacker for the round, and is the one who is prompted for attack. 
+        The Round class keeps track of which player was the original attacker, since this player must draw first regardless of whether a reflection was played.'''
+        
+        def __init__(self, p1, p2):
+            self.attacker = p1
+            self.defender = p2
+            self.attacking_card = None
+            self.card_pool = []
+            self.result = None
+
+        
+
+
     def __init__(self, p1name, p2name):
         self.player1 = HumanPlayer(p1name)
         match p2name:
@@ -142,17 +181,30 @@ class Game:
         self.deck.shuffle()
         self.deck.choose_trump()
         self.trump = self.deck.trump_card.suit
-        self.end = None
+        self.winning_player = None
 
-    def start_game(self): #TODO
+    #def start_game(self): 
 
-        while not self.end: # Game loop
+       # while not self.winning_player: # Game loop
+
             
 
 
 
 
 if __name__ == "__main__":
+    d = Deck()
+    d.shuffle()
+    d.choose_trump()
+
+    print(f"Trumps are {d.trump_card}")
+    p = HumanPlayer("Player")
+    for i in range(6):
+        p.hand.append(d.draw())
     
+    card = p.generate_move()
+    
+    print(f"You played a {card}")
+
     input('press any key to finish')
     
